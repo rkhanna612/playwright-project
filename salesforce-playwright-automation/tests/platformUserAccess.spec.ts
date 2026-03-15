6c000534d9a316112bdca6349fb181e6fca79258
Rahul Khanna <Rahul.khanna14369@gmail.com>
2026-03-15 22:03:30 +0530

    URL added and tested.

--- salesforce-playwright-automation/tests/platformUserAccess.spec.ts
+++ salesforce-playwright-automation/tests/platformUserAccess.spec.ts
@@ -3,7 +3,7 @@ const { test, expect } = require('@playwright/test');
 
 test('Platform user read only validation', async ({ page }: { page: import('@playwright/test').Page }) => {
 
-  await page.goto('/lightning/o/Opportunity/home');
+  await page.goto('https://power-connect-3925-dev-ed.scratch.lightning.force.com/lightning/o/Opportunity/home');
 
   const editButton = page.locator('button[title="Edit"]');
 
